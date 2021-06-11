1e467387f9b63f2b23794586b60ddda5fffa0b2a
bado-art <68514704+bado-art@users.noreply.github.com>
2021-06-11 16:46:40 +0000

    Update script.js
    simple interest calculator

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+.maindiv  {
+     var rate = document.getElementById("principal").value;
+     var  rate = document.getElementById("rate").value;
+     var year = document.getElementById("years").value
+     var intrest = principal * years * rate /100;
+     var year = new Date() .getFullYEAR()+parseInt (years);
+
+ }
+ fuction updateRate(){
+    var rateval = document getElementById("rate") .value;
+        document.getElementById("rate_val") .innerText=rateval;
+		}
+ 
